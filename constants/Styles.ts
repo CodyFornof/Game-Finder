4f9b3772931c22b6a7727915cf208f136bff888d
Cody Fornof <67140240+CodyFornof@users.noreply.github.com>
2026-03-21 13:01:42 -0700

    Updated styles to March 21st
    Updated styles for league components and added game details styles.

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -39,26 +39,55 @@ export const headerStyles = StyleSheet.create({
   },
   leagueContainer: {
     height: Layout.window.height * 0.06,
-    justifyContent: 'center',
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'row',
     padding: 16,
-    position: 'sticky',
+    position: 'relative',
   },
   leagueButton: {
-    height: Layout.window.height * 0.05,
+    height: Layout.window.height * 0.10,
     width: Layout.window.width * 0.16,
     justifyContent: 'center',
     alignItems: 'center',
     margin: 4,
+    paddingVertical: 12,
   },
   leagueText: {
     fontWeight: 'bold',
-    fontFamily: 'sans',
+    fontFamily: 'Arial',
     fontSize: 24,
-    color: 'white'
+    //color: '#000',
   },
+  titleContainer: {
+    flexDirection: 'row',
+    margin: 8,
+  },
+  leagueTextActive: {
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    fontSize: 24,
+    color: '#22c55e', // Green color when active
+  },
+  leagueScroll: {
+    height: 100,
+    marginVertical: 20,
+  },
+  leagueUnderline: {
+    position: 'absolute',
+    bottom: 0,
+    height: 3,
+    width: '25%', // Since there are 4 leagues
+    backgroundColor: '#22c55e',
+    borderRadius: 2,
+  },
+  leaguePressable: { // OUT OF USE, NEED TO IMPLEMENT THIS INTO CODE
+    borderTopWidth: 1, 
+    borderBottomWidth: 1, 
+    borderTopColor: '#D2D1D1', 
+    borderBottomColor: '#D2D1D1',
+    // opacity: pressed ? 0.7 : 1,  // Visual feedback when pressed
+  }
 });
 
 export const gameCard = StyleSheet.create({
@@ -76,6 +105,63 @@ export const gameCard = StyleSheet.create({
     fontWeight: 'bold',
     margin: 8,
     fontFamily: 'Arial',
+  },
+  quarterTime: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    textAlign: 'right',
+  },
+  networkName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    color: '#0EAD00',
+    alignSelf: 'center',
+  },
+});
+
+export const gameDetails = StyleSheet.create({
+  gameDetailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  gameDetails: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    flexDirection: 'column',
+  },
+  exitButton: {
+    width: 30,
+    height: 30,
+  },
+  teamLogo: {
+    height: 60,
+    width: 60,
+  },
+  teamScore: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 8,
+    fontFamily: 'Arial',
+    color: '#000',
+  },
+  networksScrollView: {
+    width: Layout.window.width * 0.80,
+    height: Layout.window.height * 0.36,
+    flexDirection: 'column',
+  },
+  networkContainer: {
+    borderTopWidth: 1,
+    borderTopColor: '#3D3D3D',
+    borderBottomWidth: 1,
+    borderBottomColor: '#3D3D3D',
+    width: Layout.window.width * 0.80,
+    height: Layout.window.height * 0.08,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
   },
 });
 
