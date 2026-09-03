52ec7b6e2f88456ecf99e6417b3fff5d8da082af
Cody Fornof <codyfornof@gmail.com>
2026-09-02 17:57:10 -0700

    Updated GmaeFinder - Submit 2 version
    Added App Icon and fixed import issues to submit the second review build.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 // 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router/react-navigation";
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
